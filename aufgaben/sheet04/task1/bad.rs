009585641c1c5a4184c36e4d3871946b8d5afef1
Tobias Weimer <wishmaster51@gmail.com>
2017-06-18 15:36:01 +0200

    Aufgabe 4.1: Programm schöner machen

--- aufgaben/sheet04/task1/bad.rs
+++ aufgaben/sheet04/task1/bad.rs
@@ -1,27 +1,29 @@
 /// print if they are both
 fn main() {
-    for iterationnumber in &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] {
-        let iterationnumber = *iterationnumber;
-        if happy_prime(iterationnumber) {
-            println!("{} is a happy prime!", iterationnumber);
+    for i in 1..21 {
+        if is_happy_prime(i) {
+            println!("{} is a happy prime!", i);
         }
     }
 }
 
 /// is it happy and prime?
-fn happy_prime(n: u32) -> bool {
-	check_if_number_is_happy(n) && check_if_number_is_prime(n)
+fn is_happy_prime(n: u32) -> bool {
+	is_happy(n) && is_prime(n)
 }
 
 /// Is it a happy number? https://en.wikipedia.org/wiki/Happy_number
-fn check_if_number_is_happy(mut number: u32) -> bool {
+fn is_happy(mut number: u32) -> bool {
     while number > 1 {
-        let mut tmp = 0;
-        while number > 0 {
-            tmp += (number %10) * (number%10);
-            number /= 10;
-        }
-        number = tmp;
+		number = {
+		    let mut sum = 0;
+		    while number > 0 {
+				let digit = number % 10;
+		        sum += digit * digit;
+		        number /= 10;
+		    }
+			sum
+        };
 
         // We ended up in a cycle -> not happy
         if number == 4 {
@@ -33,21 +35,16 @@ fn check_if_number_is_happy(mut number: u32) -> bool {
 }
 
 /// is it prime?
-fn check_if_number_is_prime(n: u32) -> bool {
-    if n == 1 || n == 2 {
+fn is_prime(n: u32) -> bool {
+    if n == 1 {
         false
     }
 	else {
-		let mut teiler_gefunden = false;
-
-		let mut teiler = 2u32;
-		while teiler < n {
-		    if n % teiler == 0 {
-		        teiler_gefunden = true;
+		for divisor in 2..n {
+		    if n % divisor == 0 {
+		        return false;
 		    }
-		    teiler = teiler + 1;
 		}
-
-		!teiler_gefunden
+		true
 	}
 }
